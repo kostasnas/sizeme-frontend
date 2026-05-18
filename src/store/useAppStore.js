eaf40d46cef07224f8d304f481cf165086903528
kostasnas <knasiovas@gmail.com>
2026-05-18 10:36:33 +0300

    Update useAppStore.js

--- src/store/useAppStore.js
+++ src/store/useAppStore.js
@@ -1,18 +1,29 @@
 import { create } from 'zustand'
 
 export const useAppStore = create((set, get) => ({
-  user: null,
-  setUser: (user) => set({ user }),
+  // Auth
+  user:      null,
+  authReady: false,          // true once Supabase has resolved session (null or user)
+  setUser:      (user)      => set({ user }),
+  setAuthReady: (authReady) => set({ authReady }),
+
+  // Profile
   profile: null,
   setProfile: (profile) => set({ profile }),
-  scans: [],
-  addScan: (scan) => set(s => ({ scans: [scan, ...s.scans] })),
+
+  // Scans
+  scans:    [],
+  addScan:  (scan)  => set(s => ({ scans: [scan, ...s.scans] })),
   setScans: (scans) => set({ scans }),
-  isPro: false,
+
+  // Pro
+  isPro:    false,
   setIsPro: (isPro) => set({ isPro }),
-  freeScansUsed: 0,
+
+  // Free scan counter
+  freeScansUsed:      0,
+  FREE_SCAN_LIMIT:    3,
   incrementFreeScans: () => set(s => ({ freeScansUsed: s.freeScansUsed + 1 })),
-  FREE_SCAN_LIMIT: 3,
   canScan: () => {
     const { isPro, freeScansUsed, FREE_SCAN_LIMIT } = get()
     return isPro || freeScansUsed < FREE_SCAN_LIMIT
